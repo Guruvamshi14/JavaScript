4fe609bc04be646869ab0a8f17ab41b7492d6cbb
guruvamshi14 <guruvamshi14@gmail.com>
2025-03-22 09:23:41 +0530

    Day 1

---
+++ Day1/script.js
@@ -0,0 +1,78 @@
+const name = "Guru vamshi"
+const PI = 3.14
+const gravity = 9.81
+
+var isMarried = false
+
+
+var challenge = "30 Days Of JavaScript"
+
+console.log(challenge)
+
+console.log(challenge.length)
+
+console.log(challenge.toUpperCase())
+
+console.log(challenge.toLowerCase())
+
+console.log(challenge.substr(0,1))
+
+console.log(challenge.substring(0,1))
+
+console.log(challenge.slice(3));
+
+console.log(challenge.includes("Script"))
+
+arr = challenge.split("");
+
+console.log(arr)
+
+arr = challenge.split(" ");
+
+console.log(arr)
+
+str = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+
+arr = str.split(", ")
+
+console.log(arr)
+
+str = "30 Days Of JavaScript JavaScript"
+
+str1 = str.replaceAll("JavaScript","Python")
+console.log(str1)
+
+console.log(str1.charAt(15));
+console.log(str1.charCodeAt(15));
+
+console.log(str1.indexOf('a'));
+console.log(str1.lastIndexOf('a'));
+
+let str2 = 'You cannot end a sentence with because because because is a conjunction'
+
+console.log(str1.indexOf('because'))
+console.log(str1.lastIndexOf('because'))
+
+console.log(str1.search('beacuse'))
+
+console.log(str1.startsWith('Hut'));
+console.log(str1.endsWith('on'));
+
+str2.concat('Hello','My name is Guru Vamshi    edbt');
+
+console.log(str2.concat('Hello','My name is Guru Vamshi    edbt'));
+
+console.log(str1.match('a'));
+
+console.log(str1.repeat(2));
+
+console.log('The quote \'There is no exercise better for the heart than reaching down and lifting people up. \' by John Holmes teaches us to help one another.')
+
+value = Math.ceil(Math.random()*100)
+console.log(value)
+
+value = 50 + Math.ceil(Math.random()*50)
+console.log(value)
+
+console.log("1 \t 1 \t 1 \t 1 \n2 \t 1 \t");
+
